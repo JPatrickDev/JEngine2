50594607c835e8f4ab29caa795406bd10311e182
JPatrickDev <jackpatrickmail@gmail.com>
2014-04-12 18:36:06 +0100

    Initial commit. Added JEngine.java with static methods to start a game, plus documentation

---
+++ src/uk/co/jdpatrick/JEngine/tests/Test.java
@@ -0,0 +1,32 @@
+package uk.co.jdpatrick.JEngine.tests;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ * Author: Jack
+ * Date: 12/04/14
+ */
+public class Test extends BasicGame{
+
+    public Test(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
---
+++ src/uk/co/jdpatrick/JEngine/tests/TestsMain.java
@@ -0,0 +1,14 @@
+package uk.co.jdpatrick.JEngine.tests;
+
+import uk.co.jdpatrick.JEngine.JEngine;
+
+/**
+ * Author: Jack
+ * Date: 12/04/14
+ */
+public class TestsMain {
+
+    public static void main(String[] args){
+        JEngine.start(new Test("JEngine tests"),800,600);
+    }
+}
